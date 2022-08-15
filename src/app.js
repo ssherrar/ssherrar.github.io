e7184295c5be771ba1c58fdb3be19d95babf7c88
Sam sherrard <105033648+ssherrar@users.noreply.github.com>
2022-08-15 16:42:46 -0500

    Second commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
+// const navigation = document.getElementById('navigation')
 
 function toggleButton() {
     navList.classList.toggle('show')
+    // navigation.classList.toggle('show')
 }
 
 hamburgerButton.addEventListener('click', toggleButton)
